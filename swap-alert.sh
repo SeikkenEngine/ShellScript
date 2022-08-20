0a702e163d2291b2e4fb41072e6c41e0d29f2527
SeikkenEngine <109022719+SeikkenEngine@users.noreply.github.com>
2022-08-20 19:55:44 +0900

    Update swap-alert.sh

--- swap-alert.sh
+++ swap-alert.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+# 남은 메모리를 감시하여 스왑 발생시 경고하는 스크립트 입니다 . 
 swapcount_limit=10                                                    #1   
  
 swapcount=$(vmstat 1 6 | awk 'NR >=4 {sum += $7 + $8} END{print sum}')#2  
