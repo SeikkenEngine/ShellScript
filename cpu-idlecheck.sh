ad2b00f5ef7c0b009888528e6af4cea5430af3df
SeikkenEngine <109022719+SeikkenEngine@users.noreply.github.com>
2022-08-20 19:33:17 +0900

    Update cpu-idlecheck.sh

--- cpu-idlecheck.sh
+++ cpu-idlecheck.sh
@@ -1,24 +1,23 @@
 #!/bin/sh
 
+# CPU 사용률 감시하는 스크립트 
+
+idle_limit=100.0  
+
+cpu_idle=$(mpstat 1 5 | tail -n 1 | awk '{print $NF}' ) 
 
 
-idle_limit=100.0  #-------------1
-
-cpu_idle=$(mpstat 1 5 | tail -n 1 | awk '{print $NF}' )  #-----------2
+is_alert=$(echo "$cpu_idle < $idle_limit" | bc )         
 
 
-is_alert=$(echo "$cpu_idle < $idle_limit" | bc )         #----------3
+if [ "$is_alert" -eq 1 ]; then                          
 
 
-if [ "$is_alert" -eq 1 ]; then                           #------------4
+    date_str=$(date '+%Y/%m/%d/%H:%M%S')               
 
 
 
-    date_str=$(date '+%Y/%m/%d/%H:%M%S')                  #----------------5
-
-
-
-    echo "[$date_str] CPU %idle Alert : $cpu_idle (%)"   #------------6
+    echo "[$date_str] CPU %idle Alert : $cpu_idle (%)"   
     /home/ec2-user/alert.sh
 
 
